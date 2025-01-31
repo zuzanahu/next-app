1e38431a97f42635d2a718f0a3755e08a06d143c
Zuzana Hubačová <hubacovazuzana8@gmail.com>
2025-01-31 17:08:51 +0100

    add document details

--- src/app/subjects/[subjectId]/[documentId]/page.tsx
+++ src/app/subjects/[subjectId]/[documentId]/page.tsx
@@ -1,7 +1,7 @@
 import Editor from "@/app/components/Editor";
 import { SetDocumentFinalButton } from "@/app/components/SetDocumentFinalButton";
 import { db } from "@/db";
-import { documentsTable } from "@/db/schema";
+import { documentsTable, usersTable } from "@/db/schema";
 import { eq } from "drizzle-orm";
 
 export default async function Page({
@@ -15,6 +15,18 @@ export default async function Page({
     where: (documentsTable, { eq }) => eq(documentsTable.id, documentId ?? ""),
   });
   const content = document?.content ?? "";
+  const ownerId = document?.ownerId;
+  // get the user's name
+  let name = "Neznámý uživatel";
+  if (ownerId) {
+    const user = await db.query.usersTable.findFirst({
+      where: eq(usersTable.id, ownerId),
+      columns: {
+        name: true,
+      },
+    });
+    name = user?.name ?? "Neznámý uživatel";
+  }
 
   async function handleSave(content: string) {
     "use server";
@@ -23,10 +35,22 @@ export default async function Page({
       .set({ content: content, revisedAt: new Date() })
       .where(eq(documentsTable.id, documentId));
   }
-
+  const time = document?.createdAt.toLocaleTimeString();
+  const date = document?.createdAt.toDateString();
+  const datetime = (date ?? "Neznámý datum") + " " + (time ?? "");
   return (
     <>
       <h1>My Page {(await params).documentId} hsshs</h1>
+      <section>
+        <h2>Informace o dokumentu</h2>
+        <ul>
+          <li>Jméno vlastníka: {name}</li>
+          <li>Datum vytvoření: {datetime}</li>
+          <li>Datum revize: {datetime}</li>
+          <li>Je dokument finalizovaný: {document?.isFinal ? "Ano" : "Ne"}</li>
+        </ul>
+      </section>
+
       <Editor initialContent={content} handleSave={handleSave}></Editor>
       <SetDocumentFinalButton documentId={documentId}></SetDocumentFinalButton>
     </>

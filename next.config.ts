6086af9352f453bca6b5ea1b627d76311c79c714
Zuzana Hubačová <hubacovazuzana8@gmail.com>
2025-04-27 10:41:41 +0200

    Add middleware and use node.js

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    nodeMiddleware: true,
+  },
 };
 
 export default nextConfig;
---
+++ src/middleware.ts
@@ -0,0 +1,42 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getUserFromSession } from "./utils/getUserFromSession";
+
+export async function middleware(request: NextRequest) {
+  const { nextUrl } = request;
+
+  if (nextUrl.pathname === "/") {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  if (nextUrl.pathname === "/login") {
+    const user = await getUserFromSession();
+
+    if (user) {
+      return NextResponse.redirect(new URL("/predmety", request.url));
+    }
+  }
+
+  if (nextUrl.pathname.startsWith("/administrace")) {
+    const user = await getUserFromSession();
+
+    // If user is not logged in and reaches for administrace page then redirect him
+    if (!user) {
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
+  }
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  // use nodejs because of connecting to the database
+  runtime: "nodejs",
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    "/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+  ],
+};

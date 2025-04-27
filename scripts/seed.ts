b383100a6639c762ef28c8a85fd904d55c579a2d
Zuzana Hubačová <hubacovazuzana8@gmail.com>
2025-04-27 11:49:51 +0200

    Create seeding logic and add initial seed data

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -1,78 +1,111 @@
 import { db } from "@/db";
-import {
-  sessionsTable,
-  usersTable,
-  type Session,
-  type User,
-} from "@/db/schema";
-import { eq } from "drizzle-orm";
-import { v4 as uuid } from "uuid";
+import * as schema from "@/db/schema";
+import fs from "fs/promises";
+import { glob } from "glob";
 
-async function insertUsersAndUpdate() {
-  const user: User = {
-    name: "John",
-    id: 1,
-    roleId: 1,
-    email: "test@gmail.com",
-    password: "test123",
-  };
-  await db.insert(usersTable).values(user);
-  console.log("New user created!");
+import { reset } from "drizzle-seed";
+import path from "path";
+await reset(db, schema);
 
-  const users = await db.select().from(usersTable);
-  console.log("Getting all users from the database: ", users);
-  /*
-    const users: {
-      id: number;
-      name: string;
-      age: number;
-      email: string;
-    }[]
-    */
-
-  await db
-    .update(usersTable)
-    .set({
-      email: "test@example.com",
-    })
-    .where(eq(usersTable.email, user.email));
-  console.log("User info updated!");
-
-  //await db.delete(usersTable).where(eq(usersTable.email, user.email));
-  //console.log('User deleted!')
+function getRandomNumber(max: number) {
+  return Math.floor(Math.random() * max);
 }
 
-async function addSessions() {
-  const user = await db.query.usersTable.findFirst({
-    where: eq(usersTable.email, "test@gmail.com"),
-  });
-  if (user) {
-    const session: Session = {
-      //vyprsi 14:15
-      expiresAt: new Date(Date.now() + 3600000 * 3),
-      //id: 12
-      userId: user.id,
-      id: uuid(),
-    };
-    await db.insert(sessionsTable).values(session);
-    console.log("New session created!");
+const [adminRole] = await db.insert(schema.userRoles).values({
+  name: "Admin",
+  canCreateDocuments: true,
+  canDeleteDocuments: true,
+  canViewUsers: true,
+});
 
-    const sessions = await db.select().from(sessionsTable);
-    console.log("Getting all sessions from the database: ", sessions);
-  }
-}
+await db
+  .insert(schema.subjectsTable)
+  .values([
+    { name: "Český jazyk" },
+    { name: "Anglický jazyk" },
+    { name: "Anglický jazyk 2" },
+    { name: "Francouzský jazyk" },
+    { name: "Fyzika" },
+    { name: "Biologie" },
+    { name: "Chemie" },
+    { name: "Dějepis" },
+    { name: "Zeměpis" },
+    { name: "Tělesná výchova" },
+    { name: "Japonský jazyk" },
+    { name: "Matematika" },
+  ]);
+console.log("Subjects created!");
 
-// Wrap the delete operation in a function
-const deleteDbs = async () => {
-  try {
-    await db.delete(sessionsTable);
-    console.log("sessions deleted!");
-    await db.delete(usersTable);
-    console.log("users deleted!");
-  } catch (error) {
-    console.error("Error deleting:", error);
-  }
-};
+const subjects = await db.query.subjectsTable.findMany();
+const subjectIds = subjects.map((subject) => subject.id);
+
+const [adminUser] = await db.insert(schema.usersTable).values({
+  name: "John",
+  roleId: adminRole.insertId,
+  email: "test@gmail.com",
+  password: "test123",
+});
+console.log("New user created!");
+
+const documentsToCreate = await glob("*.json", {
+  cwd: path.join(process.cwd(), "scripts", "documents"),
+  absolute: true,
+});
+
+await Promise.all(
+  documentsToCreate.map(async (documentJsonPath, index) => {
+    const documentJson = await fs.readFile(documentJsonPath, {
+      encoding: "utf8",
+    });
+    const document = JSON.parse(documentJson);
+
+    await db.insert(schema.documentsTable).values({
+      isFinal: true,
+      revisedAt: new Date(),
+      createdAt: new Date(),
+      subjectId: subjectIds[getRandomNumber(subjectIds.length - 1)],
+      ownerId: adminUser.insertId,
+      title: `Neznámý dokument (${index})`,
+      ...document,
+    });
+  })
+);
+
+console.log("Documents created!");
+
+process.exit();
+
+// async function addSessions() {
+//   const user = await db.query.usersTable.findFirst({
+//     where: eq(usersTable.email, "test@gmail.com"),
+//   });
+//   if (user) {
+//     const session: Session = {
+//       //vyprsi 14:15
+//       expiresAt: new Date(Date.now() + 3600000 * 3),
+//       //id: 12
+//       userId: user.id,
+//       id: uuid(),
+//     };
+//     await db.insert(sessionsTable).values(session);
+//     console.log("New session created!");
+
+//     const sessions = await db.select().from(sessionsTable);
+//     console.log("Getting all sessions from the database: ", sessions);
+//   }
+// }
+
+// // Wrap the delete operation in a function
+// const deleteDbs = async () => {
+//   try {
+//     await db.delete(sessionsTable);
+//     console.log("sessions deleted!");
+//     await db.delete(usersTable);
+//     console.log("users deleted!");
+//   } catch (error) {
+//     console.error("Error deleting:", error);
+//   }
+// };
 
 //adds one user or adds it and deletes it right away, depends if the deleting is commented out or not
 //main();

1cfe7af274abefe568c7a20b0658e3aa412043b6
Zuzana Hubačová <hubacovazuzana8@gmail.com>
2025-04-29 13:07:33 +0200

    Define not-found page handler

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+import { Title } from "@mantine/core";
+
+export default function NotFound() {
+  return (
+    <div className="container mx-auto flex justify-center items-center">
+      <Title className="!text-4xl bg-yellow-200">404 Not Found</Title>
+    </div>
+  );
+}

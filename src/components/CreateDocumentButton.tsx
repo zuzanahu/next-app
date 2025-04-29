ca7c44bbc565d37f26a2977357647df43c7d6916
Zuzana Hubačová <hubacovazuzana8@gmail.com>
2025-04-29 13:06:33 +0200

    Update subjects page

--- src/components/CreateDocumentButton.tsx
+++ src/components/CreateDocumentButton.tsx
@@ -2,6 +2,7 @@
 
 import { useTransition } from "react";
 import { createDocument } from "../server-functions/createDocument";
+import { Button } from "@mantine/core";
 
 export function CreateDocumentButton({
   subjectId,
@@ -15,9 +16,29 @@ export function CreateDocumentButton({
   textCreating: string;
 }) {
   const [isCreating, trackIsCreating] = useTransition();
+  const icon = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="size-5"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M12 4.5v15m7.5-7.5h-15"
+      />
+    </svg>
+  );
 
   return (
-    <button
+    <Button
+      justify="center"
+      leftSection={icon}
+      variant="outline"
+      color="indigo"
       onClick={() => {
         trackIsCreating(async () => {
           await createDocument(subjectId, openAfterCreate);
@@ -25,6 +46,6 @@ export function CreateDocumentButton({
       }}
     >
       {isCreating ? textCreating : text}
-    </button>
+    </Button>
   );
 }
--- src/components/ListOfSubjects.tsx
+++ src/components/ListOfSubjects.tsx
@@ -15,9 +15,11 @@ type SubjectWithDocuments = Subject & { documents: Document[] };
 export function ListOfSubjects({
   subjects,
   canCreateDocuments,
+  canDeleteDocuments,
 }: {
   subjects: SubjectWithDocuments[];
   canCreateDocuments: boolean | null | undefined;
+  canDeleteDocuments: boolean | null | undefined;
 }) {
   return (
     <Accordion
@@ -82,22 +84,22 @@ export function ListOfSubjects({
                         >
                           {document.title}
                         </Link>
-                        <Tooltip label="Duplikovat dokument" withArrow>
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth={1.5}
-                            stroke="currentColor"
-                            className="size-6"
+                        {canCreateDocuments ? (
+                          <Tooltip
+                            label="Duplikovat dokument - tato funkce ještě není implementovaná"
+                            withArrow
                           >
-                            <path
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75"
-                            />
-                          </svg>
-                        </Tooltip>
+                            <IconClipboardCopy></IconClipboardCopy>
+                          </Tooltip>
+                        ) : null}
+                        {canDeleteDocuments ? (
+                          <Tooltip
+                            label="Smazat dokument - tato funkce ještě není implementovaná"
+                            withArrow
+                          >
+                            <IconTrash></IconTrash>
+                          </Tooltip>
+                        ) : null}
                       </div>
 
                       <small>
